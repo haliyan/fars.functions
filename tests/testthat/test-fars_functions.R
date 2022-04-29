148dcda509f9bcdaf5c11c1958db63ba60addaa2
Hannah Liyanarachchi <hliyanar@yahoo.com>
2022-04-30 00:13:55 +1200

    Edited tests

--- tests/testthat/test-fars_functions.R
+++ tests/testthat/test-fars_functions.R
@@ -13,20 +13,36 @@ test_that("make_filename issues warning if it can't make a filename",{
                "NAs introduced by coercion")
 })
 
-test_that("fars_read_years correctly reads in a file", {
-  expect_warning(fars_read_years(c("2015","2025")),
-                 "invalid year: 2025")
+test_that("fars_read returns the correct tibble", {
+  expect_equal(nrow(fars_read("accident_2013.csv.bz2")), 30202)
+  expect_equal(ncol(fars_read("accident_2013.csv.bz2")), 50)
 })
 
-test_that("fars_read_years returns a list", {
+test_that("fars_read throws error if file doesn't exist",{
+  expect_error(fars_read("nonexistent_file"),
+               "file 'nonexistent_file' does not exist")
+})
+
+test_that("fars_read_years returns the correct list", {
   expect_type(fars_read_years(c("2013","2014")),
               type = "list")
+  expect_equal(length(fars_read_years(c("2013","2015"))), 2)
+  expect_equal(length(fars_read_years(c("2013","2014","2015"))), 3)
+})
+
+test_that("fars_read_years issues warnings if it can't find files", {
+  expect_warning(fars_read_years(c("2015","2025")),
+                 "invalid year: 2025")
+  expect_warning(fars_read_years(list("2014","1776")),
+                 "invalid year: 1776")
 })
 
 test_that("fars_summarize_years returns the correct list",{
   expect_type(fars_summarize_years(c("2014","2015")),
               type = "list")
-  expect_equal(nrow(fars_summarize_years("2014"))[[1]], 12)
+  expect_equal(nrow(fars_summarize_years(c("2014","2015"))), 12)
+  expect_equal(ncol(fars_summarize_years(c("2014","2015"))), 3)
+  expect_equal(ncol(fars_summarize_years(c("2013","2014","2015"))), 4)
 })
 
 test_that("fars_summarize_years issues warning if it can't find files",{
@@ -55,6 +71,3 @@ test_that("fars_map_state throws errors for states that can't be mapped",{
   expect_error(fars_map_state(2,2015),
                "nothing to draw: all regions out of bounds")
 })
-
-
-
